d9ad5a675a5f543b52a722e60fa74f4b316f0b86
Raúl Alarcón <ralarcon@microsoft.com>
2021-04-14 11:33:19 +0200

    Added ssh to custom image

---
+++ hello-world-net5/custom_start.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Get environment variables to show up in SSH session
+eval $(printenv | sed -n "s/^\([^=]\+\)=\(.*\)$/export \1=\2/p" | sed 's/"/\\\"/g' | sed '/=/s//="/' | sed 's/$/"/' >> /etc/profile)
+
+# starting sshd process
+/usr/sbin/sshd
+
+#launch app
+dotnet hello-world-net5.dll
