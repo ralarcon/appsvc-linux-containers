01bddd8085a356d12a685b515098d12fbf302c2b
Raúl Alarcón <ralarcon@microsoft.com>
2021-04-28 18:43:19 +0200

    Small change to change the last layer

--- hello-world-vue/src/components/HelloWorld.vue
+++ hello-world-vue/src/components/HelloWorld.vue
@@ -29,7 +29,7 @@
     </ul>
   </div>
   <div class="demo">
-    <p>UPDATED - START With 1 Instance!!!!</p>
+    <p>UPDATED - Other Update</p>
     <p>Using mustaches: {{ rawHtml }}</p>
     <p>Using v-html directive: <span v-html="rawHtml"></span></p>
     <p>Docker Image: {{ imageInfo }}</p>
