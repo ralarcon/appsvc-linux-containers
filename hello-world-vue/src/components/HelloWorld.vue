55659d93dc23569ac38d50e66b25feac7b2b4cc8
Raúl Alarcón <ralarcon@microsoft.com>
2021-04-28 12:54:52 +0200

    Update 2

--- hello-world-vue/src/components/HelloWorld.vue
+++ hello-world-vue/src/components/HelloWorld.vue
@@ -29,7 +29,7 @@
     </ul>
   </div>
   <div class="demo">
-    <p>UPDATED!!!!</p>
+    <p>UPDATED 2!!!!</p>
     <p>Using mustaches: {{ rawHtml }}</p>
     <p>Using v-html directive: <span v-html="rawHtml"></span></p>
     <p>Docker Image: {{ imageInfo }}</p>
